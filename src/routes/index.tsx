76eae80b8c6b8cd6081a6ee8fee7dd5ec9fef7d1
unknown <Camila Tavares>
2023-11-07 13:28:07 +0100

    feat/creating login page and changing the window icon

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/Home/HomePage";
 import Register from "../pages/RegisterPage/register";
+import Login from "../pages/Login/login";
 
 export const RoutesMain = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
       <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
