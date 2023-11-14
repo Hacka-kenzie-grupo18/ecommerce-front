4c0a150c3b4711823ca09eb05637ece854712e2f
Jozhué Beni <jbeeniif@gmail.com>
2023-11-14 10:57:13 -0300

    feat:create modal default and dashboard organization

--- src/pages/Dashboard/DashboardAdmin.tsx
+++ src/pages/Dashboard/DashboardAdmin.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import AsideAdmin from "./components/AsideAdmin";
+import HeaderAdmin from "./components/HeaderAdmin";
+import MainAdmin from "./components/MainAdmin";
 
 const DashboardAdmin = () => {
   return (
-    <div className="h-[1200px]">
+    <div className="h-[950px] flex">
       <AsideAdmin />
+      <div className="flex flex-col  w-full">
+        <HeaderAdmin />
+        <MainAdmin />
+      </div>
     </div>
   );
 };
--- src/pages/Dashboard/components/AsideAdmin.tsx
+++ src/pages/Dashboard/components/AsideAdmin.tsx
@@ -1,45 +1,54 @@
-import React from "react";
 import { BiUserPlus } from "react-icons/bi";
 import { FaUserGear, FaBuildingUser } from "react-icons/fa6";
 import { PiFolderSimpleUserFill } from "react-icons/pi";
 
 const AsideAdmin = () => {
   return (
-    <aside className="w-[25%] bg-orange-400 h-full">
-      <div className="h-[80px] w-full flex justify-center items-center bg-green-400">
-        <h2 className="text-[25px] font-bold ">Culture Nerd</h2>
+    <aside className="w-[25%] bg-primary100 shadow-aside-dashboard h-full">
+      <div className="h-[100px] w-full flex justify-center border-b-2 items-center bg-primary">
+        <h2 className="text-[32px] text-white  font-bold ">Culture Nerd</h2>
       </div>
-      <div className="flex justify-center flex-col  items-center h-fit mt-2">
-        <button className="flex rounded justify-between px-4 w-[70%] text-[18px] font-bold bg-green-300 h-12 items-center">
-          <span className=" font-bold text-[25px]">
-            <BiUserPlus />
-          </span>
-          Criar produto
-        </button>
-      </div>
-      <div className="flex justify-center flex-col  items-center h-fit mt-2">
-        <button className="flex rounded justify-between px-4 w-[70%] text-[18px] font-bold bg-green-300 h-12 items-center">
-          <span className=" font-bold text-[22px]">
-            <FaUserGear />
-          </span>
-          Editar Usuário
-        </button>
-      </div>
-      <div className="flex justify-center flex-col  items-center h-fit mt-2">
-        <button className="flex rounded justify-between px-4 w-[70%] text-[18px] font-bold bg-green-300 h-12 items-center">
-          <span className=" font-bold text-[22px]">
-            <FaBuildingUser />
-          </span>
-          Pág. Disponíveis
-        </button>
-      </div>
-      <div className="flex justify-center flex-col  items-center h-fit mt-2">
-        <button className="flex rounded justify-between px-4 w-[70%] text-[18px] font-bold bg-green-300 h-12 items-center">
-          <span className=" font-bold text-[22px]">
-            <PiFolderSimpleUserFill />
-          </span>
-          Estoques
-        </button>
+      <div className="gap-16 flex flex-col">
+        <div className="flex justify-center   items-center pt-12  h-fit ">
+          <button className="flex rounded shadow-aside-dashboard border-2  justify-between px-4 w-[70%] text-[18px] font-bold bg-grey300 h-12 items-center">
+            <span className=" font-bold  text-[25px]">
+              <BiUserPlus />
+            </span>
+            Criar produto
+          </button>
+        </div>
+        <div className="flex justify-center  items-center h-fit ">
+          <button className="flex rounded shadow-aside-dashboard border-2  justify-between px-4 w-[70%] text-[18px] font-bold bg-grey300 h-12 items-center">
+            <span className=" font-bold text-[22px]">
+              <FaUserGear />
+            </span>
+            Editar Usuário
+          </button>
+        </div>
+        <div className="flex justify-center   items-center h-fit ">
+          <button className="flex rounded  shadow-aside-dashboard border-2  justify-between px-4 w-[70%] text-[15px] font-bold bg-grey300 h-12 items-center">
+            <span className=" font-bold text-[22px]">
+              <FaBuildingUser />
+            </span>
+            Pág. Disponíveis
+          </button>
+        </div>
+        <div className="flex justify-center   items-center h-fit ">
+          <button className="flex rounded shadow-aside-dashboard border-2  justify-between px-4 w-[70%] text-[18px] font-bold bg-grey300 h-12 items-center">
+            <span className=" font-bold text-[22px]">
+              <PiFolderSimpleUserFill />
+            </span>
+            + Estoques
+          </button>
+        </div>
+        <div className="flex justify-center   items-center h-fit ">
+          <button className="flex rounded shadow-aside-dashboard border-2  justify-between px-4 w-[70%] text-[18px] font-bold bg-grey300 h-12 items-center">
+            <span className=" font-bold text-[22px]">
+              <PiFolderSimpleUserFill />
+            </span>
+            - Estoques
+          </button>
+        </div>
       </div>
     </aside>
   );
