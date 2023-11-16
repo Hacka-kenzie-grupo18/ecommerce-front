554c0b6e095d9ede166c1106372f8c3276cc7f3f
unknown <Camila Tavares>
2023-11-16 12:52:27 +0100

    newsletter

--- src/pages/ProductPage/index.tsx
+++ src/pages/ProductPage/index.tsx
@@ -1,15 +1,15 @@
 import DescriptionAndComment from '../../Components/DescriptionAndComment/indext'
+import Footer from '../../Components/Footer'
 import Product from '../../Components/Product'
 import RelatedProducts from '../../Components/RelatedProducts'
 
-const ProductPage = () => {
-  return (
-    <div className='bg-grey200 h-[100%] flex flex-col items-center'>
-      <Product />
-      <DescriptionAndComment />
-      <RelatedProducts />
-    </div>
-  )
-}
+const ProductPage = () => (
+  <div className='bg-grey200 h-[100%] flex flex-col items-center'>
+    <Product />
+    <DescriptionAndComment />
+    <RelatedProducts />
+    <Footer />
+  </div>
+)
 
 export default ProductPage
